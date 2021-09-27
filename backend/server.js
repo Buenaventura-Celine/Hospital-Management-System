beeeb369a9cdbd0ecc2d4c5a853591af96bf222c
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-27 12:35:50 +0800

    Connection to database

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,17 @@ const port = process.env.PORT || 5000; //5000 as default or kahit anong port na 
 app.use(cors()); //gagamitin na yung cors dun sa application
 app.use(express.json()); //kasi di nakadefault na json format yung mga data sa node js
 
+//mongodb connection
+//sinet sa true yung ibang object kasi hindi ayun yung nakadefault
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+//execution of connection
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection is established')
+})
+
 app.listen(port, () => {
     console.log(`Server is running in port ${port}`)
 })
