3d1fa49d26202718682d0e5597a3ed9a894abde8
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-29 17:44:15 +0800

    Installing bootstrap and react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
   return (
